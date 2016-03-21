37f1143cef7515b91cce6642f3e014b4aa3f5868
chris clarke <cieclarke@gmail.com>
2016-03-21 10:09:50 +0000

    fig caption

--- js/CC.Photos.js
+++ js/CC.Photos.js
@@ -4,7 +4,22 @@ CC.Photos = (function () {
        
     var getListURL = "https://api.flickr.com/services/rest/?method=flickr.photosets.getList&user_id=67828456@N07&api_key=61777036f4ecf11adb192f7156c6e92e&per_page=10&nojsoncallback=1&format=json";
 
+    var fancyOptions = {
+                                'openEffect': 'elastic',
+                                'closeEffect': 'elastic',
+                                'nextEffect': 'fade',
+                                'openSpeed': 600, 
+                                'closeSpeed': 200,
+                                'maxWidth': '500',
+                                'autoResize': true
+                                }  ;
+
     var Init = function () {
+
+        if($j(".jumbotron").find(".container.photos").length <= 0)
+        {
+            return;
+        }
 
         $j(".jumbotron").find(".container.photos").empty();
         $j("body").append("<div id='fancy-box-hidden' class='hidden' />");
@@ -37,12 +52,20 @@ CC.Photos = (function () {
                
                     $j(".jumbotron").find(".container").find('#' + rowid).append(
                         '<div id='+photosetID+' class="col-md-4 col-xs-12 col-sm-4">' +
-                            '<a class="fancybox" rel="'+photosetID+'" href="https://www.flickr.com/photos/cieclarke/albums/' + photosetID + '">' +
-                                '<figure>' +
-                                    '<img src="https://farm'+this.farm+'.staticflickr.com/'+this.server+'/'+this.primary+'_'+this.secret+'_m.jpg" />' +
-                                '<figcaption>'+title+'</figcaption>' +
-                                '</figure>' +
-                            '</a>' +
+                            '<figure>' +
+                                '<img src="https://farm'+this.farm+'.staticflickr.com/'+this.server+'/'+this.primary+'_'+this.secret+'_m.jpg" />' +
+                                '<figcaption>' +
+                                    '<div>' + title + '</div>' +
+                                    '<div>' + 
+                                    '<a  class="fancybox" rel="'+photosetID+'s" href="https://www.flickr.com/photos/cieclarke/albums/' + photosetID + '">' +
+                                        'Slideshow' +
+                                    '</a>' +
+                                    ' <a  id="album' + photosetID + 'a" rel="'+photosetID+'a" href="#album' + photosetID + '">' +
+                                        'Album' +
+                                    '</a>' +
+                                    '</div>' +
+                                '</figcaption>' +
+                            '</figure>' +
                         '</div>'
                     );
                     
@@ -59,31 +82,32 @@ CC.Photos = (function () {
                         },
                         complete: function (data) {
                             var D = $j.parseJSON(data.responseText);
+                            $j("body").append('<div id="album'+photosetID+'" style="display: none;"></div>');
                             $j(D.photoset.photo).each(function() {
+                                
                                 photos.push({
                                     href: 'https://farm'+this.farm+'.staticflickr.com/'+this.server+'/'+this.id+'_'+this.secret+'_z.jpg',
                                     title: this.title
                                 });
+                                
+                                $j("body").find("#album"+photosetID).append(
+                                    '<img src="https://farm'+this.farm+'.staticflickr.com/'+this.server+'/'+this.id+'_'+this.secret+'_z.jpg" />'
+                                );
+                                
                             });
+                            
+                            $j('#album'+photosetID+'a').fancybox();
+                            
                         }
             
                     });
                     
-                    $j('#' + photosetID).find('a').click(function(e){
+                    $j('#' + photosetID).find('a[rel="'+photosetID+'s"]').click(function(e){
                         e.preventDefault();
-                        
-                        $j.fancybox.open(
-                            photos,
-                            {
-                            'openEffect': 'elastic',
-                            'closeEffect': 'elastic',
-                            'nextEffect': 'fade',
-                            'openSpeed': 600, 
-                            'closeSpeed': 200
-                            }  
-                        );
-                        
+                        $j.fancybox.open(photos, fancyOptions); 
                     });
+                    
+                    
                     
                     i++;  
                     
--- js/CC.js
+++ js/CC.js
@@ -5,8 +5,9 @@ var $j = jQuery.noConflict();
 //Site namespce
 var CC = CC || {};
 
+
 $j(document).ready(function () {
 
     CC.Photos.Init();
-
+    
 });
