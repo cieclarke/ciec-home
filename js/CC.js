db8d32928fa489786c5226619e4f5eaa03001103
chris clarke <cieclarke@gmail.com>
2016-04-05 13:11:00 +0100

    routing configured

--- js/CC.js
+++ js/CC.js
@@ -2,14 +2,15 @@ var $j = jQuery.noConflict();
 
 var CC = CC || {};
 
-CC.Constants = {
+CC.Global = {
     ALBUM_URL: 
          'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&primary_photo_extras=url_s' +
                 '&user_id=67828456@N07&api_key=61777036f4ecf11adb192f7156c6e92e&per_page=10&nojsoncallback=1&format=json',
     PHOTOS_URL: function(albumId){
         return 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&user_id=67828456@N07&api_key=61777036f4ecf11adb192f7156c6e92e&' + 
-                'photoset_id='+albumId+'&nojsoncallback=1&format=json';
-    }
+                'photoset_id='+albumId+'&nojsoncallback=1&format=json&extras=url_m,url_s';
+    },
+    Router: function(){}
     
 }
 
@@ -38,6 +39,25 @@ CC.Models = {
             return this.attributes.description._content;
         }
         
+    }),
+    
+    Photo: Backbone.Model.extend({
+        
+        initialize: function() {
+            
+            _.bindAll(this, 'Id', 'Url');
+            
+        },
+        Id: function() {
+            return this.attributes.id;
+        },
+        Url: function() {
+            return this.attributes.url_m;
+        },
+        Title: function() {
+            return this.attributes.title;
+        }
+        
     })
 
 }
@@ -48,11 +68,46 @@ CC.Collections = {
     
         model: CC.Models.Album,
     
+    }),
+    
+    PhotoList: Backbone.Collection.extend({
+    
+        model: CC.Models.Photo,
+    
     })
 
 }
 
 CC.Views = {
+
+    BackView: Backbone.View.extend({
+        tagName: 'ol',
+        className: 'breadcrumb',
+        render: function(){
+            
+            var count = this.collection.length;
+            var el = this.$el;
+            
+            var listItems = _.map(this.collection, function(item, index) {
+                
+                var active = '';
+                
+                if(index + 1 == count) {
+                    return '<li class="active">'+item+'</li>';
+                }
+                else {
+                    return '<li class="active"><a href="#home">'+item+'</a></li>';
+                }
+             
+            });
+            
+            _.each(listItems, function(item) {
+                $j(el).append($j(item));
+            })
+            
+            return this;
+        }
+    }),
 
     AlbumView: Backbone.View.extend({
         
@@ -64,8 +119,8 @@ CC.Views = {
         events:  {
             'click  a.nav' : function(e) {
                 e.preventDefault();
-                console.log('click');  
-                //router.navigate(e.target.hash, true); 
+                console.log(e.target.hash);
+                CC.Global.Router.navigate(e.target.hash, true); 
             }
         },
         render: function(){
@@ -81,8 +136,7 @@ CC.Views = {
 
     AlbumsView: Backbone.View.extend({
         initialize: function(){
-            
-            
+
         },
         
         tagName: 'ul',
@@ -104,7 +158,7 @@ CC.Views = {
 
     PhotosView: Backbone.View.extend({
         initialize: function(){
-            console.log(this.collection);
+            
         },
 
         tagName: 'ul',
@@ -126,7 +180,7 @@ CC.Views = {
 
     PhotoView: Backbone.View.extend({
         initialize: function(){
-            console.log(this.collection);
+            
         },
 
         tagName: 'li',
@@ -143,41 +197,65 @@ CC.Views = {
 
 }
 
+CC.Router = {
 
+    AlbumRouter: Backbone.Router.extend({
+        
+        albumList: null,
+        albumView: null,
+        albumsView: null,
+        photoList: null,
+        photoView: null,
+        
+        initialize: function() {
+            console.log('router');
+            //_.bindAll(this, 'albumList', 'albumView');
+            
+            $j.getJSON(CC.Global.ALBUM_URL,
+            {}, 
+            function(data){
+                this.albumList = new CC.Collections.AlbumList(data.photosets.photoset);
+                this.albumsView = new CC.Views.AlbumsView({collection: this.albumList});
+                this.backView = new CC.Views.BackView({collection: ['Albums']});
+                $j(".jumbotron").find(".container.photos").empty();
+                $j(".breadcrumb").remove();
+                $j(".jumbotron").find(".container.photos").append(this.backView.render().el);
+                $j(".jumbotron").find(".container.photos").append(this.albumsView.render().el);
+            });
+        
+        },
+        
+        routes: {
+            'home': 'index',
+            'album/:id': 'album'
+        },
 
-var AlbumRouter = Backbone.Router.extend({
-    routes: {
-        '': 'index',
-        'album/:id': 'album'
-    },
-
-    album: function(id){
-        loadAlbum(id);
-    },
-    
-    index: function() {
-        loadIndex();
-    },
-    
-    showAlbum : function(id){ 
-        loadAlbum(id);
-    },
-    
-    showAlbums: function(){
-        loadIndex();
-    }
-
-});
-
+        album: function(id){
+            //$j(".jumbotron").find(".container.photos").html('<p>album route: '+id+'</p>');
+            $j.getJSON(CC.Global.PHOTOS_URL(id),
+            {}, 
+            function(data){
+                this.photoList = new CC.Collections.PhotoList(data.photoset.photo);
+                this.photosView = new CC.Views.PhotosView({collection: this.photoList});
+                this.backView = new CC.Views.BackView({collection: ['Albums', data.photoset.id]});
+                $j(".jumbotron").find(".container.photos").empty();
+                $j(".breadcrumb").remove();
+                $j(".jumbotron").find(".container.photos").append(this.backView.render().el);
+                $j(".jumbotron").find(".container.photos").append(this.photosView.render().el);
+            });
+        },
+        
+        index: function() {
+            
+            this.initialize();
+           
+        }
+    })
+};
 
 $j(document).ready(function () {   
 
-    $j.getJSON(CC.Constants.ALBUM_URL,
-    {}, 
-    function(data){
-        var albumList = new CC.Collections.AlbumList(data.photosets.photoset);
-        var albumsView = new CC.Views.AlbumsView({collection: albumList});
-        $j(".jumbotron").find(".container.photos").html(albumsView.render().el);
-    });
-
+    CC.Global.Router = new CC.Router.AlbumRouter();
+    Backbone.history.start({ pushState: false });
+    
 });
