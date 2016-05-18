426c3b4c1a7d98e9b4acc3c92b5c05eaf98949d4
Chris Clarke <cieclarke@gmail.com>
2016-05-18 09:31:59 +0100

    Homepage view updates

--- js/CC.Blog.js
+++ js/CC.Blog.js
@@ -14,8 +14,9 @@ CC.Blog.Models = {
     Blog: Backbone.Model.extend({
         
         initialize: function() {
-            //_.bindAll(this, 'Title');
-            console.log(this.attributes.title);
+            _.bindAll(this, 'Title', 'Type', 'Thumbnail', 'Summary');
+            //console.log(this.attributes.title);
+            //console.log(this.attributes.type);
         },
         Title : function(){
             if(this.attributes.title == undefined) {
@@ -23,7 +24,28 @@ CC.Blog.Models = {
             } else {
                 return this.attributes.title;
             }
+        },
+        Type : function(){
+            return this.attributes.type;
+        },
+        Summary : function(){
+            return this.attributes.summary;
+        },
+        Thumbnail: function(){
+            if(this.attributes.photos == undefined) {
+                return '';
+            } else {
+                if(this.attributes.photos[0].alt_sizes == undefined)
+                {
+                    return '';
+                }
+                else {
+                    return this.attributes.photos[0].alt_sizes[4].url;    
+                }
+            }
+             
         }
+        
         
     })
 
@@ -50,17 +72,29 @@ CC.Blog.Views = {
         className: 'list-group',
         render: function(){
             
-            var tmpl = _.template($j('#blogs').html());
-            var $dd = this.$el;
+            //var tmpl = _.template($j('#blogs').html());
+            var $element = this.$el;
             _.each(this.collection.models, function(item){
                 
-                $dd.append(tmpl(item));    
+                switch(item.Type())
+                {
+                    case 'link':
+                        var tmpl = _.template($j('#blog-list-link').html());
+                        $element.append(tmpl(item));
+                        break;
+                    case 'photo':
+                        var tmpl = _.template($j('#blog-list-photo').html());
+                        $element.append(tmpl(item));
+                        break;
+                    case 'text':
+                        var tmpl = _.template($j('#blog-list-text').html());
+                        $element.append(tmpl(item));                        
+                        break;
+                }
                 
-
-                //$j(el).append(photoView.render().el);
+                
+                //$element.append(tmpl(item));
             });
-            
-            //this.$el.html(tmpl(this.model));
         
             return this;
         }    
@@ -69,26 +103,15 @@ CC.Blog.Views = {
 }
 
 $j(document).ready(function () {   
-
-    /*
-    $j.getJSON(CC.Blog.Global.TUMBLR_URL,
-            {}, 
-            function(data){
-                var blogList = new CC.Blog.Collections.BlogList(data.response.posts);
-                var blogsView = new CC.Blog.Views.BlogsView({collection: blogList});
-                $j(".jumbotron").find(".container.home").empty();    
-                $j(".jumbotron").find(".container.home").append(this.blogsView.render().el);
-            });
-    */        
-            
-            $j.ajax({
-    url: CC.Blog.Global.TUMBLR_URL,
-    dataType: 'jsonp',
-    success: function(data){
-                var blogList = new CC.Blog.Collections.BlogList(data.response.posts);
-                var blogsView = new CC.Blog.Views.BlogsView({collection: blogList});
-                $j(".jumbotron").find(".container.home").empty();    
-                $j(".jumbotron").find(".container.home").append(blogsView.render().el);
-    }
+         
+    $j.ajax({
+        url: CC.Blog.Global.TUMBLR_URL,
+        dataType: 'jsonp',
+        success: function(data){
+            var blogList = new CC.Blog.Collections.BlogList(data.response.posts);
+            var blogsView = new CC.Blog.Views.BlogsView({collection: blogList});
+            $j(".jumbotron").find(".container.home").empty();    
+            $j(".jumbotron").find(".container.home").append(blogsView.render().el);
+        }
     });
 });
